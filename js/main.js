10dacbd9e0e7d6c3e9bc1b9c7874f6048a45a9ef
Ttima <tanjanett@mail.ru>
2017-05-20 19:07:36 +0300

    slider-home

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.foto').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<img src="img/Layer 10 copy.png">',
+  nextArrow: '<img src="img/Layer 10.png">'
+  });
+
+});
+
